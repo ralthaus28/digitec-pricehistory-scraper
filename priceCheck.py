6021e0fcc7cf132c5ef231947f1e491997d50991
retoalt28 <retoalt28@icloud.com>
2024-04-22 20:59:36 +0200

    removed userdata

--- priceCheck.py
+++ priceCheck.py
@@ -23,7 +23,7 @@ def check_price_drop(productId):
 
     # Request headers
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+        'User-Agent': 'My-Scraper',
     }
 
     # Send the POST request
--- pricetrendPlot.py
+++ pricetrendPlot.py
@@ -25,7 +25,7 @@ payload = {
 
 # Anforderungs-Header
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+    'User-Agent': 'My-Scraper',
 }
 
 # Senden der POST-Anfrage
